2b2cef5ea566efb511ad2a7e18fe828dff75a186
Eyal Cohen (Networking) <eyal.cohen01@audiocodes.com>
2025-03-18 13:09:05 +0200

    Change test_server.py

--- test_server.py
+++ test_server.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-def send_request(text, url="http://127.0.0.1:5000/predict"):
+def send_request_to_D_Nikud(text, url="http://127.0.0.1:5000/predict"):
     headers = {"Content-Type": "application/json"}
     data = json.dumps({"text": text})
 
@@ -10,10 +10,12 @@ def send_request(text, url="http://127.0.0.1:5000/predict"):
 
     if response.status_code == 200:
         print("Response:", response.json())
+        return response.json()["diacritized_text"]
     else:
         print("Error:", response.status_code, response.text)
 
 
 if __name__ == "__main__":
     sample_text = "האם בתאריך עשרים וחמישה ביוני, ביום שני, בשעה ארבע ארבעים וחמש, במרפאה ברחוב הנביאים 2, חיפה, יתאים לכם תור אצל דוקטור אביטל, מומחה לרפואת עיניים?"
-    send_request(sample_text)
+    out = send_request_to_D_Nikud(sample_text)
+    print(out)
